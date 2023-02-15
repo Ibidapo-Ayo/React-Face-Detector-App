3e1532e4b69672f59348e9bb5df0aa895b5f6847
Ibidapo Ayomide Victor <ibidapoayomide754@gmail.com>
2023-02-15 02:26:44 +0100

    Navbar component added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="shadow flex flex-col md:flex-row py-10 px-20 w-full items-center justify-end">
+           <button className="bg-blue px-3 py-2">Signup</button>
+        </div>
+    )
+}
+
+export default Navbar
